27c5a4028e56db8f5696480dc0af7733d8a42a1d
iscojara <iscojara14@gmail.com>
2022-05-05 16:06:45 -0500

    raaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ js/general.js
@@ -0,0 +1,41 @@
+const paginas=[
+    {
+        nombre:"PRINCIPAL",
+        enlace:"index.html",
+        subpagina:[]
+    },
+    {
+        nombre:"PROCESO DE INICIO", 
+        enlace:"#",
+        subpagina:[
+            {
+                nombre: "DESARROLLAR EL ACTA DE LA CONSTITUCIÓN DEL PROYECTO",
+                enlace: "fase-inicio1.html"
+            }
+        ]
+    },
+    {
+        nombre: "EN PROCESO",
+        enlace: "#",
+        subpagina:[]
+    },  
+    {
+        nombre: "EN PROCESO",
+        enlace: "#",
+        subpagina:[]
+    }
+];
+var resultado="";
+/* llenar los links en el nav*/
+for(var i=0;i<paginas.length;i++){
+    if(i==0){
+        resultado+="<li><a href='"+paginas[i].enlace+"'>"+paginas[i].nombre+"</a></li>";
+    }else{
+        resultado+="<li><a href='"+paginas[i].enlace+"'>"+paginas[i].nombre+"</a><ul>";
+        for(var j=0;j<paginas[i].subpagina.length;j++){
+            resultado+="<li><a href='"+paginas[i].subpagina[j]['enlace']+"'>"+paginas[i].subpagina[j]['nombre']+"</a><li>";
+        }
+        resultado+="</ul></li>";
+    }    
+}
+document.getElementById("nav").innerHTML=resultado;
